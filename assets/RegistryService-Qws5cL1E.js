2a8f447ec7e2abeac7d5206964912ed421c222ce
hasToDev <hasdikus@gmail.com>
2026-04-18 16:40:07 +0700

    fix: DistributePage amount coercion + demo whitelist deploy

---
+++ assets/RegistryService-Qws5cL1E.js
@@ -0,0 +1,1 @@
+import{a,b as t,i as s}from"./provider-DLyWKNEh.js";const n="InvestorRegistry",e=a.investorRegistry;async function c(){return t(e,s,"investorCount",[],n)}async function g(r,o){return t(e,s,"getInvestorsPaginated",[r,o],n)}export{g,c as i};
