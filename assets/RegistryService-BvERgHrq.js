196593464dae4e11b0e3e5be5c9bf48ef6556998
hasToDev <hasdikus@gmail.com>
2026-05-04 20:04:27 +0700

    update landing

--- assets/RegistryService-BvERgHrq.js
+++
@@ -1,1 +0,0 @@
-import{dv as r,dw as d}from"./index-Qir-Trh1.js";import{c as e}from"./provider-DjuLIgv8.js";const s="InvestorRegistry",o=d.investorRegistry;async function g(t){return e(o,r,"holderCount",[t],s)}async function u(t,n,a){return e(o,r,"getHoldersPaginated",[t,n,a],s)}export{u as g,g as h};
