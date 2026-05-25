81da9b5e38e00f9c41d1920152a726c0aeb9fadc
hasToDev <hasdikus@gmail.com>
2026-05-25 11:26:50 +0700

    deploy: frontend v0.3.86 - Slice 1 MCP sell: scoped mint carries redeem + queue-submit caps + buys&sells consent copy

---
+++ assets/useWallet-1o81-Gmf.js
@@ -0,0 +1,1 @@
+import{de as o,df as c}from"./index-BBzk8erJ.js";function a(){const e=o(),{address:n,connecting:t,connected:s,error:r}=c(e);return{address:n,connecting:t,connected:s,error:r,connect:e.connect,register:e.register,disconnect:e.disconnect,signMessage:e.signMessage,sendUserOperation:e.sendUserOperation,getViemClients:e.getViemClients,tryReconnect:e.tryReconnect}}export{a as u};
