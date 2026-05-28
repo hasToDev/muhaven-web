7398e6697351e0b55f5d502230408da0f6d3e9e1
hasToDev <hasdikus@gmail.com>
2026-05-28 20:33:31 +0700

    deploy: frontend v0.3.87 - W3 CashPage Withdraw + cash.unwrap MCP target

---
+++ assets/RegistryService-DfuWviRd.js
@@ -0,0 +1,1 @@
+import{dC as r,dD as d}from"./index-GHSk-9h_.js";import{c as e}from"./provider-CMm9D94F.js";const s="InvestorRegistry",o=d.investorRegistry;async function g(t){return e(o,r,"holderCount",[t],s)}async function u(t,n,a){return e(o,r,"getHoldersPaginated",[t,n,a],s)}export{u as g,g as h};
