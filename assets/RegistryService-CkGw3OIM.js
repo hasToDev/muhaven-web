2c518f989230ff5234d92fc35ba5087d2b207ec2
hasToDev <hasdikus@gmail.com>
2026-06-01 15:26:02 +0700

    deploy: Apply-issuer stepper 411px overflow fix (v0.3.124)

--- assets/RegistryService-CkGw3OIM.js
+++
@@ -1,1 +0,0 @@
-import{dC as r,dD as d}from"./index-BANW9Vhr.js";import{c as e}from"./provider-vhLWI1iq.js";const s="InvestorRegistry",o=d.investorRegistry;async function g(t){return e(o,r,"holderCount",[t],s)}async function u(t,n,a){return e(o,r,"getHoldersPaginated",[t,n,a],s)}export{u as g,g as h};
